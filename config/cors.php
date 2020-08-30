e5a28af74d0f08a509ab476134587b219622da3b
Aldo <aldodumitrescu@gmail.com>
2020-08-30 20:37:40 +0300

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://northpoll.findaldo.dev'],
 
     'allowed_origins_patterns' => [],
 
